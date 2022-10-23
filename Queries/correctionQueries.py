d62e4b375ac05cd18cf9260205f9ffd2850d3156
Aleksandr Martynov <77721717+hardsmoke@users.noreply.github.com>
2022-10-23 21:26:34 +1000

    Update correctionQueries.py

--- Queries/correctionQueries.py
+++ Queries/correctionQueries.py
@@ -22,31 +22,17 @@ city_idCity, drivetrain_idDrivetrain, transmission_idTransmission) VALUES
 (:ownerId, :mileage, :releaseYear, :color, :price, :photo, :description, :horsePowers, :modelId, :bodyTypeId, :cityId, :drivetrainId, :transmissionId)'''
 
 cursor.execute(addCarQuery, 
-{'ownerId': 1, 'mileage': 235, 'releaseYear': 2009,
+{'ownerId': 12, 'mileage': 235, 'releaseYear': 2009,
 'color': '', 'price': 250000, 'photo': '', 'description': '',
 'horsePowers': 115, 'modelId': 21, 'bodyTypeId': 2,
 'cityId': 10, 'drivetrainId': 1, 'transmissionId': 1})
-
-cursor.execute(addCarQuery, 
-{'ownerId': 1, 'mileage': 235235, 'releaseYear': 2015,
-'color': '', 'price': 540000, 'photo': '', 'description': '',
-'horsePowers': 300, 'modelId': 23, 'bodyTypeId': 2,
-'cityId': 10, 'drivetrainId': 1, 'transmissionId': 2})
-
-cursor.execute(addCarQuery, 
-{'ownerId': 1, 'mileage': 32853, 'releaseYear': 2019,
-'color': '', 'price': 1090000, 'photo': '', 'description': '',
-'horsePowers': 512, 'modelId': 25, 'bodyTypeId': 2,
-'cityId': 10, 'drivetrainId': 1, 'transmissionId': 2})
 
 # Добавить объявление
 addAdQuery = '''
 INSERT INTO advertisment(owner_idOwner, car_idCar, applicationDate) VALUES
 (:ownerId, :carId, (SELECT DATE()))'''
 
-cursor.execute(addAdQuery, {'ownerId': 1, 'carId': 1})
-cursor.execute(addAdQuery, {'ownerId': 1, 'carId': 2})
-cursor.execute(addAdQuery, {'ownerId': 1, 'carId': 3})
+cursor.execute(addAdQuery, {'ownerId': 12, 'carId': 27})
 
 selectAllAds = '''SELECT * FROM advertisment'''
 selectAllOwners = '''SELECT * FROM owner'''
